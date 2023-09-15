9a249aa014158e1694ea05baa6fee0c1e081bdc6
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-09-15 22:03:02 +0530

    /src_updated

--- src/instance.js
+++ src/instance.js
@@ -2,7 +2,8 @@ import Web3 from 'web3';
 import Cert from './Cert.json' assert { type: 'json' };
 import deployer from './deployer.json' assert { type: 'json' };
 
-export const web3 = new Web3('http://127.0.0.1:8545');
+const provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
+export const web3 = new Web3(provider);
 export const myContract = new web3.eth.Contract(
   Cert.abi,
   Cert.networks['5777'].address,
